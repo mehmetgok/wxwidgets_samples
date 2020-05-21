2503bb65a75e72ba553cc10d4fae8d6efcbc8fee
Mehmet GOK <mehmetgkk@gmail.com>
2020-05-21 22:26:26 +0300

    Ek işlevler uygulandı (glOrtho)

--- minimal_gl/minimal_gl/glcanvas.cpp
+++ minimal_gl/minimal_gl/glcanvas.cpp
@@ -12,20 +12,31 @@ wxGLCanvasSubClass::wxGLCanvasSubClass(wxFrame *parent, int* args)
 }
 
 
+wxGLCanvasSubClass::~wxGLCanvasSubClass()
+{
+	delete m_context;
+}
+
 void wxGLCanvasSubClass::Paintit(wxPaintEvent& WXUNUSED(event)) {
 	Render();
 }
 
 void wxGLCanvasSubClass::Render()
 {
+	// if (!IsShown()) return;
+
 	SetCurrent(*m_context);
 	wxPaintDC(this);
 	
 	glClearColor(0.0, 0.0, 0.0, 0.0);
 	glClear(GL_COLOR_BUFFER_BIT);
-	glViewport(0, 0, (GLint)GetSize().x, (GLint)GetSize().y);
+	// glViewport(0, 0, (GLint)GetSize().x, (GLint)GetSize().y);
 
-	glBegin(GL_POLYGON);
+	 // ------------- draw some 2D ----------------
+	prepare2DViewport(0, 0, getWidth() / 2, getHeight());
+	glLoadIdentity();
+
+	/*glBegin(GL_POLYGON);
 	glColor3f(1.0, 1.0, 1.0);
 	glVertex2f(-0.5, -0.5);
 	glVertex2f(-0.5, 0.5);
@@ -33,6 +44,14 @@ void wxGLCanvasSubClass::Render()
 	glVertex2f(0.5, -0.5);
 	glColor3f(0.4, 0.5, 0.4);
 	glVertex2f(0.0, -0.8);
+	glEnd();*/
+
+	glColor4f(1, 0, 0, 1);
+	glBegin(GL_QUADS);
+	glVertex3f(getWidth() / 8, getHeight() / 3, 0);
+	glVertex3f(getWidth() * 3 / 8, getHeight() / 3, 0);
+	glVertex3f(getWidth() * 3 / 8, getHeight() * 2 / 3, 0);
+	glVertex3f(getWidth() / 8, getHeight() * 2 / 3, 0);
 	glEnd();
 
 
@@ -41,4 +60,41 @@ void wxGLCanvasSubClass::Render()
 
 	glFlush();
 	SwapBuffers();
+}
+
+/** Inits the OpenGL viewport for drawing in 2D. */
+void wxGLCanvasSubClass::prepare2DViewport(int topleft_x, int topleft_y, int bottomrigth_x, int bottomrigth_y)
+{
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Black Background
+	glEnable(GL_TEXTURE_2D);   // textures
+	glEnable(GL_COLOR_MATERIAL);
+	glEnable(GL_BLEND);
+	glDisable(GL_DEPTH_TEST);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	glViewport(topleft_x, topleft_y, bottomrigth_x - topleft_x, bottomrigth_y - topleft_y);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	gluOrtho2D(topleft_x, bottomrigth_x, bottomrigth_y, topleft_y);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
+void wxGLCanvasSubClass::resized(wxSizeEvent& evt)
+{
+	//	wxGLCanvas::OnSize(evt);
+
+	Refresh();
+}
+
+
+int wxGLCanvasSubClass::getWidth()
+{
+	return GetSize().x;
+}
+
+int wxGLCanvasSubClass::getHeight()
+{
+	return GetSize().y;
 }
--- minimal_gl/minimal_gl/glcanvas.h
+++ minimal_gl/minimal_gl/glcanvas.h
@@ -3,6 +3,16 @@
 #include <wx/wx.h>
 #include <wx/glcanvas.h>
 
+
+// include OpenGL
+#ifdef __WXMAC__
+#include "OpenGL/glu.h"
+#include "OpenGL/gl.h"
+#else
+#include <GL/glu.h>
+#include <GL/gl.h>
+#endif
+
 #ifndef WIN32
 #include <unistd.h> // FIXME: ¿This work/necessary in Windows?
 //Not necessary, but if it was, it needs to be replaced by process.h AND io.h
@@ -12,7 +22,19 @@ class wxGLCanvasSubClass : public wxGLCanvas {
 	void Render();
 public:
 	wxGLContext*	m_context;
+	
 	wxGLCanvasSubClass(wxFrame* parent, int* args);
+	virtual ~wxGLCanvasSubClass();
+
+	void resized(wxSizeEvent& evt);
+
+	void prepare2DViewport(int topleft_x, int topleft_y, int bottomrigth_x, int bottomrigth_y);
+
+	
+	int getWidth();
+	int getHeight();
+
+
 	void Paintit(wxPaintEvent& event);
 protected:
 	DECLARE_EVENT_TABLE()
